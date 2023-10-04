41ba2981c4da52753d8435951cbe7f2fc1e8446e
Jachob Dolak <dolak.jachob@gmail.com>
2023-10-03 22:22:54 -0400

    added links to index

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('index.html')
+@app.route("/devplan")
+def devplan():
+    return render_template('devplan.html')
  
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0', port=80)
