2cbd313fb19aeba4ca3e00e3348ba2a145d8663c
Jachob Dolak <dolak.jachob@gmail.com>
2023-10-03 20:11:12 -0400

    got flask to work and added homepage

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ from flask import Flask
 # instance of flask application
 app = Flask(__name__)
  
-# home route that returns below text
-# when root url is accessed
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+def index():
+    return render_template('index.html')
  
 if __name__ == '__main__':
-    app.run(debug=True, port=80)
+    app.run(debug=True,host='0.0.0.0', port=80)
