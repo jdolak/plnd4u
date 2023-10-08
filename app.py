cccd22f88e6ddec63dbfb7061a824d129463b164
Calista Annelie Suwita <calistasuwita@dhcp-10-12-239-26.dhcp.nd.edu>
2023-10-08 15:28:36 -0400

    website up

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, url_for
 
 # instance of flask application
 app = Flask(__name__)
@@ -6,6 +6,27 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/home")
+def home():
+    css_url = url_for('static', filename='css/styles.css')
+    return render_template('home.html', css_url=css_url)
+
+# @app.route("/classes")
+# def classes():
+#     return render_template('classes.html')
+
+# @app.route("/plan")
+# def plan():
+#     return render_template('plan.html')
+
+# @app.route("/login")
+# def login():
+#     return render_template('login.html')
+
+# @app.route("/register")
+# def login():
+#     return render_template('register.html')
  
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0', port=80)
