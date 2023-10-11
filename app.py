ba14bd20d2cec3ed05dfd257dd18a2be668242aa
Calista Annelie Suwita <calistasuwita@dhcp-10-12-129-110.dhcp.nd.edu>
2023-10-10 20:27:52 -0400

    Login Page: Done

--- app.py
+++ app.py
@@ -13,23 +13,24 @@ def home():
     return render_template('home.html', css_url=css_url)
 
 @app.route("/register")
-def login():
+def register():
     css_url = url_for('static', filename='css/styles.css')
     return render_template('register.html', css_url=css_url)
 
-# @app.route("/classes")
-# def classes():
-#     return render_template('classes.html')
+@app.route("/classes")
+def classes():
+    css_url = url_for('static', filename='css/styles.css')
+    return render_template('classes.html', css_url=css_url)
 
-# @app.route("/plan")
-# def plan():
-#     return render_template('plan.html')
+@app.route("/plan")
+def plan():
+    css_url = url_for('static', filename='css/styles.css')
+    return render_template('plan.html', css_url=css_url)
 
-# @app.route("/login")
-# def login():
-#     return render_template('login.html')
+@app.route("/login")
+def login():
+    css_url = url_for('static', filename='css/styles.css')
+    return render_template('login.html', css_url=css_url)
 
-
- 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0', port=80)
