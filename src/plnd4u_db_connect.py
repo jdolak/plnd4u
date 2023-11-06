0e3986dfdf256bd3a13f72b33d256c626955c2c5
Jachob Dolak <dolak.jachob@gmail.com>
2023-11-06 01:26:21 -0500

    added keepalive and exponetial backoff to db connection

--- src/plnd4u_db_connect.py
+++ src/plnd4u_db_connect.py
@@ -4,6 +4,9 @@ import os
 import mysql.connector
 from dotenv import load_dotenv
 import logging
+import threading
+import time
+from retry import retry
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 LOG = logging.getLogger()
@@ -11,21 +14,42 @@ LOG = logging.getLogger()
 load_dotenv()
 DB_PASSWD = os.getenv('MYSQL_ROOT_PASSWORD')
 
-cwd = os.getenv('PWD')
 
-if cwd == "/plnd4u" or cwd == None:
-    DB = mysql.connector.connect(
-        host="plnd4u-db-1",
-        port="3306",
-        user="root",
-        password=DB_PASSWD,
-        database="plnd4u"
+
+@retry(delay=0.5, backoff=2)
+def _db_establish_connection():
+    global DB
+    cwd = os.getenv('PWD')
+    if cwd == "/plnd4u" or cwd == None:
+        DB = mysql.connector.connect(
+            host="plnd4u-db-1",
+            port="3306",
+            user="root",
+            password=DB_PASSWD,
+            database="plnd4u"
+        )
+    else:
+        DB = mysql.connector.connect(
+            host="localhost",
+            port="3306",
+            user="root",
+            password=DB_PASSWD,
+            database="plnd4u"
     )
-else:
-    DB = mysql.connector.connect(
-        host="localhost",
-        port="3306",
-        user="root",
-        password=DB_PASSWD,
-        database="plnd4u"
-    )
+
+def _db_keep_alive(length):
+    while True:
+        try:
+            mycursor = DB.cursor()
+            sql = "SELECT NOW()"
+            mycursor.execute(sql)
+            LOG.info(f"Keep alive success: {list(mycursor)}")
+        except Exception as e:
+            LOG.error(f"Keepalive failed: {e}")
+
+        time.sleep(length)
+
+_db_establish_connection()
+
+db_keep_alive_thread = threading.Thread(target=_db_keep_alive, args=(7200,), daemon=True)
+db_keep_alive_thread.start()
