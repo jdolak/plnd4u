3930fa15566dca1d9e4809cd45b7904c3635144e
Jachob Dolak <dolak.jachob@gmail.com>
2023-11-23 21:34:19 -0500

    phone commit didnt work, this does tho

--- src/func_basic.py
+++ src/func_basic.py
@@ -24,8 +24,8 @@ def db_enroll_class(netid, course_id, sem, title):
 
 
 def db_check_class_in_enrollment(netid, course_id, sem, title, deleted):
-
-    sql = "SELECT enrollment_id FROM has_enrollment WHERE netid = %s AND course_id = %s AND sem = %s AND title LIKE '%s%' AND deleted = %s"
+    title = f"{title[0:(len(title)-3)]}%"
+    sql = "SELECT enrollment_id FROM has_enrollment WHERE netid = %s AND course_id = %s AND sem = %s AND title LIKE %s AND deleted = %s"
     val = (netid, course_id, sem, title, deleted)
     try:
         mycursor = DB.cursor()
