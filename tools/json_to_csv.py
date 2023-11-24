71922c39808ea4f54b029c17ba5e8d99df10c0b2
Sam Martin <smarti39@nd.edu>
2023-11-24 17:22:03 -0500

    modification to json converter

--- tools/json_to_csv.py
+++ tools/json_to_csv.py
@@ -3,12 +3,13 @@
 import json
 import csv
 
-with open("../data/descriptions.json", encoding='utf-8') as fd:
+with open("data/descriptions.json", encoding='utf-8') as fd:
     json_data = json.load(fd)
 
-with open("../data/descriptions.tsv", "w", newline="") as csvfile:
+with open("data/descriptions.tsv", "w", newline="") as csvfile:
     writer = csv.writer(csvfile, delimiter="\t")
 
     for course in json_data:
         desc = " ".join(json_data[course].splitlines())
+        desc = desc.replace("\t", " ")
         writer.writerow([f'{course}', f'{desc}'])
