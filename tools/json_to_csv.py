6c5c5fea75ca1ba6932448ce0767aa91840cdb0e
Samuel Martin <smarti39@nd.edu>
2023-11-25 23:58:27 -0500

    formatted sp24 data into csv for insertion

--- tools/json_to_csv.py
+++ tools/json_to_csv.py
@@ -3,19 +3,17 @@
 import json
 import csv
 
-def convert(source, dest):
-    with open(source, encoding='utf-8') as fd:
-        json_data = json.load(fd)
-
-    with open(dest, "w", newline="") as csvfile:
-        writer = csv.writer(csvfile, delimiter=",")
-
-        for course in json_data:
-            coreq = json_data[course]
-            writer.writerow([f'{course}', f'{coreq}'])
-        
 def main():
-    convert("data/credits.json", "data/credits.csv")
+    with open("data/json/sp24_overview.json", encoding='utf-8') as fd:
+        json_data = json.load(fd)
+        overview = json_data["results"]
+
+    with open("data/csv/sp24_overview.csv", "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, delimiter=",")
+        writer.writerow(["code","title","crn","meets","professor","start_date"])
+
+        for cd in overview:
+            writer.writerow([cd["code"],cd["title"],cd["crn"],cd["meets"],cd["instr"],cd["start_date"]])
 
 if __name__=="__main__":
     main()
