675ffe0e74165556dccf5f38fe0f96be4b4f0a3e
Sam Martin <smarti39@nd.edu>
2023-12-02 15:15:08 -0500

    test modified

--- src/test.py
+++ src/test.py
@@ -2,7 +2,15 @@
 
 from func_basic import *
 
-course = "MATH 10550"
+course_code = "KSGA 10001"
 
-print(db_search_past_classes(course, (1,1,1,1,1,1,1,1,1)))
+desc = db_show_description(course_code)
+credits = db_show_credits(course_code)
+core_reqs = db_show_core_reqs(course_code)
+recent_sems = db_show_semesters(course_code)
+profs = db_show_profs(course_code)
+meeting_times = db_show_meeting_times(course_code)
+coreqs = db_show_coreqs(course_code)
+prereqs = db_show_prereqs(course_code)
 
+print(f'{desc} \n{credits} \n{core_reqs} \n{recent_sems} \n{profs} \n{meeting_times} \n{coreqs} \n{prereqs}')
