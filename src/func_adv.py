cf3e6e5c498a0dc493560dce9acbd1185929cc19
amyers25-dev <83961050+amyers25-dev@users.noreply.github.com>
2023-12-03 21:30:56 -0500

    Advanced functions for checking pre and corequisites

--- src/func_adv.py
+++ src/func_adv.py
@@ -7,6 +7,7 @@ from func_basic import *
 
 # Advanced Function: Secure login
 
+
 def db_create_login(netid, password):
     """Generates and inserts a salt and hash for a new user
     :returns: 1 on failure, 0 on success"""
@@ -22,7 +23,7 @@ def db_create_login(netid, password):
         # invalid password
         LOG.error(f"Invalid password: {password} : {e}")
         return 1
-    
+
     # generate salt and salted hash
     salt_bytes = secrets.token_bytes(32)
     h = hashlib.new("sha256")
@@ -42,6 +43,7 @@ def db_create_login(netid, password):
         LOG.error(f"Failed to create login : {e}")
         return 1
 
+
 def db_check_login(netid, password_attempt):
     """Checks if hash of login exists in database
     RETURN VALUES
@@ -50,7 +52,7 @@ def db_check_login(netid, password_attempt):
         2   error
         3   netid not matched
     """
-    
+
     # obtain salt and hash from DB if they exist
     mycursor = DB.cursor()
 
@@ -67,7 +69,7 @@ def db_check_login(netid, password_attempt):
     if len(result) == 0:
         LOG.error("Netid not in login table")
         return 3
-    
+
     # if salt hash pair found, convert to bytes
     salt_bytes = bytes.fromhex(result[0][0])
     hash_correct = bytes.fromhex(result[0][1])
@@ -79,7 +81,7 @@ def db_check_login(netid, password_attempt):
         # invalid password
         LOG.error(f"Password check : {e}")
         return 2
-    
+
     h = hashlib.new("sha256")
     h.update(password_attempt_bytes)
     h.update(salt_bytes)
@@ -93,9 +95,10 @@ def db_check_login(netid, password_attempt):
     LOG.info(f"Login success: logged in {netid}")
     return 0
 
+
 def register_student(netid, name, major_code, gradyear, password):
     """
-    Abstraction function that resisters a student by added them to the 
+    Abstraction function that resisters a student by added them to the
     student database and generates password hash
 
     RETURN VAULES:
@@ -106,7 +109,7 @@ def register_student(netid, name, major_code, gradyear, password):
     if db_register_student(netid, name, major_code, gradyear):
         LOG.error("Failed to register student.")
         return 1
-    if db_create_login(netid,password):
+    if db_create_login(netid, password):
         LOG.error("Failed to create login.")
         return 2
     LOG.info(f"Student registration success: {netid}")
@@ -115,25 +118,85 @@ def register_student(netid, name, major_code, gradyear, password):
 
 # Advanced Function: Validate Major Requirements
 
+
 def mike_ryan(netid):
-    """Runs necessary functions to check if all major requirements 
+    """Runs necessary functions to check if all major requirements
     fulfilled (named by Prof. Weninger)
     :returns: list of unfulfilled courses and core reqs"""
+
 
 def db_check_required_courses(netid):
     """Cross-references a student's major requirements with their enrollments
     :returns: list of required courses unfulfilled in enrollments"""
     pass
 
+
 def db_check_core_requirements(netid):
     """Cross-references a student's core requirements with their enrollments
     :returns: list of required core reqs unfulfilled in enrollments"""
     pass
 
+
 # Advanced Function: Validate Requisite Reuirements
 
-def db_check_course_conflicts(netid, enrollment_id):
+
+def db_check_corequisites(netid):
     """Cross-references an enrollment's coreqs and prereqs with
     the student's other enrollments
     :returns: false if there exist needed coreqs or prereqs not in has_enrollments"""
-    pass
+
+    missing = {}
+
+    try:
+        with DB.cursor() as mycursor:
+            sql = """
+            SELECT H.netid, H.course_id, R.coreq_id, H.sem 
+            FROM has_enrollment H, course_has_coreq R 
+            WHERE netid = %s AND H.course_id = R.course_id;
+            """
+            mycursor.execute(sql, (netid,))
+            corequisites = list(mycursor)
+
+            for _, code, coreq, sem in corequisites:
+                mycursor.execute("SELECT * from has_enrollment WHERE course_id = %s and sem = %s", (coreq, sem))
+                if not list(mycursor):
+                    missing.setdefault(code, []).append(coreq)
+
+        return missing if missing else None
+
+    except Exception as e:
+        LOG.error(e)
+        return 1
+
+
+def db_check_prerequisites(netid):
+    """Cross-references an enrollment's coreqs and prereqs with
+    the student's other enrollments
+    :returns: false if there exist needed coreqs or prereqs not in has_enrollments"""
+
+    semesters = ["FRFA", "FRSP", "SOFA", "SOSP", "JUFA", "JUSP", "SEFA", "SESP"]
+    missing = {}
+
+    try:
+        with DB.cursor() as mycursor:
+            sql = """
+            SELECT H.netid, H.course_id, R.prereq_ids, H.sem 
+            FROM has_enrollment H, course_has_prereq R 
+            WHERE netid = %s AND H.course_id = R.course_id;
+            """
+            mycursor.execute(sql, (netid,))
+            prerequisites = list(mycursor)
+
+            for _, code, prereqs, sem in prerequisites:
+                for req in prereqs.split(","):
+                    mycursor.execute("SELECT sem from has_enrollment WHERE course_id = %s AND deleted = 0;", (req,))
+                    semesters_taken = [sem[0] for sem in mycursor]
+
+                    if not semesters_taken or semesters.index(semesters_taken[0]) >= semesters.index(sem):
+                        missing.setdefault(code, []).append(req)
+
+        return missing if missing else None
+
+    except Exception as e:
+        LOG.error(e)
+        return 1
